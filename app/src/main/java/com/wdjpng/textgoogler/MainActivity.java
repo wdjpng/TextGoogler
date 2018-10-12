65a3d6a23144acec2d8b2e200e141b7b9bd3af84
wdjpng <muenzel.lukas@gmail.com>
2018-10-12 17:36:02 +0200

    added feature to take pictures or select them from the gallery

--- app/src/main/java/com/wdjpng/textgoogler/MainActivity.java
+++ app/src/main/java/com/wdjpng/textgoogler/MainActivity.java
@@ -1,13 +1,104 @@
 package com.wdjpng.textgoogler;
 
+import android.content.Intent;
+import android.graphics.Bitmap;
+import android.net.Uri;
+import android.provider.MediaStore;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+
+import java.io.IOException;
 
 public class MainActivity extends AppCompatActivity {
+
+    static private final int REQUEST_IMAGE_CAPTURE = 1;
+    static private final int REQUEST_IMAGE_PICK = 2;
+
+    private Button selectImageButton;
+    private Button takeImageButton;
+
+    private Bitmap imageBitmap;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        defineWidgets();
+        defineOnClickListeners();
+
+    }
+
+    private void defineWidgets() {
+        selectImageButton = this.findViewById(R.id.selectImageButton);
+        takeImageButton = this.findViewById(R.id.takeImageButton);
+    }
+
+    private void defineOnClickListeners() {
+        selectImageButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                dispatchSelectPictureIntent();
+            }
+        });
+
+        takeImageButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                dispatchTakePictureIntent();
+            }
+        });
+    }
+
+    @Override
+    protected void onActivityResult(int requestCode, int resultCode, Intent data) {
+
+        try {
+
+            if (requestCode == REQUEST_IMAGE_CAPTURE && resultCode == RESULT_OK) {
+                Bundle extras = data.getExtras();
+                imageBitmap = (Bitmap) extras.get("data");
+
+            }
+
+            else if (requestCode == REQUEST_IMAGE_PICK && resultCode == RESULT_OK && data != null && data.getData() != null) {
+
+                Uri uri = data.getData();
+
+                imageBitmap = MediaStore.Images.Media.getBitmap(getContentResolver(), uri);
+
+            }
+
+
+        }
+
+        catch (IOException | NullPointerException e ) {
+            e.printStackTrace();
+        }
+
+    }
+
+    private void dispatchTakePictureIntent() {
+        Intent takePictureIntent = new Intent(MediaStore.ACTION_IMAGE_CAPTURE);
+        if (takePictureIntent.resolveActivity(getPackageManager()) != null) {
+            startActivityForResult(takePictureIntent, REQUEST_IMAGE_CAPTURE);
+        }
+    }
+
+
+    private void dispatchSelectPictureIntent() {
+        Intent getIntent = new Intent(Intent.ACTION_GET_CONTENT);
+        getIntent.setType("image/*");
+
+        Intent pickIntent = new Intent(Intent.ACTION_PICK);
+        pickIntent.setType("image/*");
+
+        Intent chooserIntent = Intent.createChooser(getIntent, getString(R.string.selectImage));
+        chooserIntent.putExtra(Intent.EXTRA_INITIAL_INTENTS, new Intent[]{pickIntent});
+
+        startActivityForResult(chooserIntent, REQUEST_IMAGE_PICK);
     }
 }
+
