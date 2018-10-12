24a7b775dbbfa7c3e14d8041e82b9a39ceb94b4d
wdjpng <muenzel.lukas@gmail.com>
2018-10-12 19:46:43 +0200

    Added basic text recognition features

--- app/src/main/java/com/wdjpng/textgoogler/MainActivity.java
+++ app/src/main/java/com/wdjpng/textgoogler/MainActivity.java
@@ -2,14 +2,29 @@ package com.wdjpng.textgoogler;
 
 import android.content.Intent;
 import android.graphics.Bitmap;
+import android.graphics.Point;
+import android.graphics.Rect;
 import android.net.Uri;
 import android.provider.MediaStore;
+import android.support.annotation.NonNull;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.view.View;
 import android.widget.Button;
 
+import com.google.android.gms.tasks.OnFailureListener;
+import com.google.android.gms.tasks.OnSuccessListener;
+import com.google.android.gms.vision.text.Element;
+import com.google.android.gms.vision.text.Line;
+import com.google.android.gms.vision.text.TextBlock;
+import com.google.firebase.ml.vision.FirebaseVision;
+import com.google.firebase.ml.vision.common.FirebaseVisionImage;
+import com.google.firebase.ml.vision.text.FirebaseVisionText;
+import com.google.firebase.ml.vision.text.FirebaseVisionTextRecognizer;
+import com.google.firebase.ml.vision.text.RecognizedLanguage;
+
 import java.io.IOException;
+import java.util.List;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -19,7 +34,8 @@ public class MainActivity extends AppCompatActivity {
     private Button selectImageButton;
     private Button takeImageButton;
 
-    private Bitmap imageBitmap;
+    private OnFailureListener textRecogniserOnFailureListener;
+    private OnSuccessListener textRecogniserOnSuccessListener;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -27,7 +43,7 @@ public class MainActivity extends AppCompatActivity {
         setContentView(R.layout.activity_main);
 
         defineWidgets();
-        defineOnClickListeners();
+        defineListeners();
 
     }
 
@@ -36,7 +52,7 @@ public class MainActivity extends AppCompatActivity {
         takeImageButton = this.findViewById(R.id.takeImageButton);
     }
 
-    private void defineOnClickListeners() {
+    private void defineListeners() {
         selectImageButton.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
@@ -48,12 +64,56 @@ public class MainActivity extends AppCompatActivity {
             @Override
             public void onClick(View v) {
                 dispatchTakePictureIntent();
+
+
             }
         });
+
+        textRecogniserOnFailureListener = new OnFailureListener() {
+            @Override
+            public void onFailure(@NonNull Exception e) {
+
+            }
+        };
+
+        textRecogniserOnSuccessListener = new OnSuccessListener() {
+            @Override
+            public void onSuccess(Object o) {
+                FirebaseVisionText result = (FirebaseVisionText) o;
+
+                String resultText = result.getText();
+                for (FirebaseVisionText.TextBlock block: result.getTextBlocks()) {
+                    String blockText = block.getText();
+                    Float blockConfidence = block.getConfidence();
+                    List<RecognizedLanguage> blockLanguages = block.getRecognizedLanguages();
+                    Point[] blockCornerPoints = block.getCornerPoints();
+                    Rect blockFrame = block.getBoundingBox();
+                    for (FirebaseVisionText.Line line: block.getLines()) {
+                        String lineText = line.getText();
+                        Float lineConfidence = line.getConfidence();
+                        List<RecognizedLanguage> lineLanguages = line.getRecognizedLanguages();
+                        Point[] lineCornerPoints = line.getCornerPoints();
+                        Rect lineFrame = line.getBoundingBox();
+                        for (FirebaseVisionText.Element element: line.getElements()) {
+                            String elementText = element.getText();
+                            Float elementConfidence = element.getConfidence();
+                            List<RecognizedLanguage> elementLanguages = element.getRecognizedLanguages();
+                            Point[] elementCornerPoints = element.getCornerPoints();
+                            Rect elementFrame = element.getBoundingBox();
+                        }
+                    }
+                }
+
+                System.out.println(resultText);
+            }
+        };
+
+
     }
 
     @Override
     protected void onActivityResult(int requestCode, int resultCode, Intent data) {
+        Bitmap imageBitmap = null;
 
         try {
 
@@ -71,7 +131,9 @@ public class MainActivity extends AppCompatActivity {
 
             }
 
-
+            if(imageBitmap != null){
+                recogniseText(imageBitmap);
+            }
         }
 
         catch (IOException | NullPointerException e ) {
@@ -99,6 +161,18 @@ public class MainActivity extends AppCompatActivity {
         chooserIntent.putExtra(Intent.EXTRA_INITIAL_INTENTS, new Intent[]{pickIntent});
 
         startActivityForResult(chooserIntent, REQUEST_IMAGE_PICK);
+    }
+
+    private void recogniseText(Bitmap imageBitmap){
+        FirebaseVisionImage image = FirebaseVisionImage.fromBitmap(imageBitmap);
+
+        FirebaseVisionTextRecognizer textRecognizer = FirebaseVision.getInstance()
+                .getOnDeviceTextRecognizer();
+
+        textRecognizer.processImage(image)
+                .addOnSuccessListener(textRecogniserOnSuccessListener).
+                addOnFailureListener(textRecogniserOnFailureListener);
+
     }
 }
 
