7eb93d0827825c64bfbfb0c4f9f0942a022747e3
wdjpng <muenzel.lukas@gmail.com>
2018-10-13 18:40:36 +0200

    Improved UI

--- app/src/main/java/com/wdjpng/textgoogler/MainActivity.java
+++ app/src/main/java/com/wdjpng/textgoogler/MainActivity.java
@@ -3,12 +3,14 @@ package com.wdjpng.textgoogler;
 import android.content.Intent;
 import android.graphics.Bitmap;
 import android.net.Uri;
+import android.provider.ContactsContract;
 import android.provider.MediaStore;
 import android.support.annotation.NonNull;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.view.View;
 import android.widget.Button;
+import android.widget.ImageButton;
 
 import com.google.android.gms.tasks.OnFailureListener;
 import com.google.android.gms.tasks.OnSuccessListener;
